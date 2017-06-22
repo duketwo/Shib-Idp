129ff9c99e5873fc430c14371b1ee9f8971de401
Daniel Bader <dbader@stud.hs-heilbronn.de>
2017-06-22 22:03:18 +0200

    Refactored populate.sh to use environment variables and properly use volume mounted directories for persistence.

--- ldap/populate.sh
+++ ldap/populate.sh
@@ -2,23 +2,32 @@
 set -e
 first_run=true
 
-if [[ -f "/ldap_populated" ]]; then 
+if [[ -f "/etc/ldap/ldap_populated" ]]; then 
     first_run=false
 fi
 
 # Wait until 'LDAP started and listens on port 389'.
 while [ -z "`netstat -tln | grep 389`" ]; do
-  echo 'Waiting for PostgreSQL to start ...'
+  echo 'Waiting for LDAP to start ...'
   sleep 1
 done
 echo 'LDAP started.'
+
+dc_string=""
+IFS="."; declare -a dc_parts=($SLAPD_DOMAIN); unset IFS
+
+for dc_part in "${dc_parts[@]}"; do
+	dc_string="$dc_string,dc=$dc_part"
+done
+
+echo "DC string: $dc_string"
 
 if [[ "$first_run" == "true" ]]; then
 	echo "Loading prepopulate data."
 	if [[ -d "/etc/ldap/prepopulate" ]]; then 
 		for file in `ls /etc/ldap/prepopulate/*.ldif`; do
-			ldapadd -x -D cn=admin,dc=shib -w toor -f "$file"
+			ldapadd -x -D cn=admin$dc_string -w $SLAPD_PASSWORD -f "$file"
 		done
 	fi
-	touch /ldap_populated
+	touch /etc/ldap/ldap_populated
 fi
