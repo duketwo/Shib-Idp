997d5d82110f45f503f2ef739a5a3d6086ea2577
Daniel Bader <dbader@stud.hs-heilbronn.de>
2017-07-05 19:23:19 +0200

    Status quo

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ rm -r ./config/ldap
 rm ./docker-compose.yml
 cp ./defaultconf/docker-compose.yml.default ./docker-compose.yml
 rm ./haproxy/haproxy.cfg
-cp ./defaultconf/haproxy/haproxy.cfg.default ./haproxy/haproxy.cfg
+cp ./defaultconf/haproxy.cfg.default ./haproxy/haproxy.cfg
 docker-compose run --rm -v "$(pwd)/config/idp:/ext-mount" idp init-idp.sh
 rm ./config/idp/conf/ldap.properties
 cp ./defaultconf/ldap.properties.default ./config/idp/conf/ldap.properties
