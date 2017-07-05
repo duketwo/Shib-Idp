35a254e5ddf2fb0a9bb7e686a9ac0c060765e09f
Daniel Bader <dbader@stud.hs-heilbronn.de>
2017-07-05 18:24:54 +0200

    Proper file names, removed IPs from docker-compose

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 rm -r ./config/idp
 rm -r ./config/ldap
 rm ./docker-compose.yml
-cp ./docker-compose.yml.example ./docker-compose.yml
+cp ./docker-compose.yml.default ./docker-compose.yml
 rm ./haproxy/haproxy.cfg
-cp ./haproxy/haproxy.cfg.example ./haproxy/haproxy.cfg
+cp ./haproxy/haproxy.cfg.default ./haproxy/haproxy.cfg
