e211313d59bdbe7e291dcb2b5bad4de1d27b1b88
Daniel Bader <dbader@stud.hs-heilbronn.de>
2017-10-06 06:51:17 +0200

    Added docker-compose build cmd to init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ rm -r ./config/ldap
 rm -r ./config/mysql
 rm ./docker-compose.yml
 cp ./defaultconf/docker-compose.yml.default ./docker-compose.yml
+docker-compose build
 docker-compose run --rm -v "$(pwd)/config/idp:/ext-mount" idp init-idp.sh
 rm ./config/idp/conf/ldap.properties
 cp ./defaultconf/ldap.properties.default ./config/idp/conf/ldap.properties
