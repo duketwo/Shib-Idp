e203cc7b0d35a6b8e20f3201804e76a123249d70
Daniel Bader <dbader@stud.hs-heilbronn.de>
2017-10-06 04:58:31 +0200

    Removed HAProxy

--- README.md
+++ README.md
@@ -17,7 +17,6 @@ Shib-Idp
 
 ## Featuring
  - Jetty as Servlet/JSP-Container und Webserver for Shibboleth
- - HAProxy as proxy middlware including SSL offloading
  - phpLDAPadmin to administrate the LDAP directory
  - OpenLDAP as directory service for Shibboleth
  - Shibbleth IdP provides Single Sign-On services
--- init.sh
+++ init.sh
@@ -4,8 +4,6 @@ rm -r ./config/ldap
 rm -r ./config/mysql
 rm ./docker-compose.yml
 cp ./defaultconf/docker-compose.yml.default ./docker-compose.yml
-rm ./haproxy/haproxy.cfg
-cp ./defaultconf/haproxy.cfg.default ./haproxy/haproxy.cfg
 docker-compose run --rm -v "$(pwd)/config/idp:/ext-mount" idp init-idp.sh
 rm ./config/idp/conf/ldap.properties
 cp ./defaultconf/ldap.properties.default ./config/idp/conf/ldap.properties
